695b3d267d4983f52121ce89d3bc589e9abcedb1
o-az <23618431+o-az@users.noreply.github.com>
2022-05-17 02:06:36 -0500

    generate site map

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,6 @@
 /** @type {import('@types/prettier').Config} */
+const { resolve } = require('path')
+
 module.exports = {
   semi: false,
   tabWidth: 2,
@@ -11,13 +13,20 @@ module.exports = {
   bracketSpacing: true,
   bracketSameLine: false,
   quoteProps: 'as-needed',
-  // plugins: ['prettier-plugin-tailwindcss'],
-  // tailwindConfig: './tailwind.config.js',
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       options: {
         parser: 'typescript',
+      },
+    },
+    {
+      files: ['*.xml'],
+      options: {
+        parser: 'html',
+        xmlSelfClosingSpace: true,
+        htmlWhitespaceSensitivity: 'ignore',
+        plugins: [`${resolve(__dirname + '/node_modules')}/@prettier/plugin-xml`],
       },
     },
   ],
---
+++ scripts/generate-sitemap.ts
@@ -0,0 +1,43 @@
+import { writeFile } from 'fs/promises'
+import globby from 'globby'
+import prettier from 'prettier'
+
+async function generateSiteMap() {
+  try {
+    const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
+    const pages = await globby(['**/src/routes/**/*.tsx', '!**/node_modules/**'])
+    const siteMap = `
+  <?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+      .map(page => {
+        const path = page
+          .replace(/^src\//, '')
+          .replace(/\.tsx$/, '')
+          .replace('.mdx', '')
+          .replace('.html', '')
+          .replace('routes', '')
+          .replace('/index', '')
+        const route = path === '/index' ? '' : path
+        return `
+        <url>
+          <loc>
+          https://omaraziz.dev${route}
+          </loc>
+        </url>
+      `
+      })
+      .join('')}
+  </urlset>
+  `
+    const formatted = prettier.format(siteMap, { ...prettierConfig, parser: 'html' })
+    await writeFile('public/sitemap.xml', formatted)
+    return true
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : `Encoutered an error: ${error}`
+    console.trace(errorMessage)
+    return false
+  }
+}
+
+generateSiteMap().then(_ => console.log(JSON.stringify(_, null, 2)))
