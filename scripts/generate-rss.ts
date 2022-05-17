b5ba884945844ce551b44aa46fc934bbbf1a0108
o-az <23618431+o-az@users.noreply.github.com>
2022-05-17 02:15:26 -0500

    rss

---
+++ scripts/generate-rss.ts
@@ -0,0 +1,23 @@
+import { writeFile } from 'fs/promises'
+import RSS from 'rss'
+
+/**
+ * INPORTANT: this won't be implemented until contentlayer is available for vite
+ * @see {@link https://www.contentlayer.dev/docs/environments/vite}
+ */
+async function generateRSS() {
+  try {
+    const feed = new RSS({
+      title: 'Omar Aziz',
+      site_url: 'https://omaraziz.dev',
+      feed_url: 'https://omaraziz.dev/feed.xml',
+    })
+    return true
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : `Encoutered an error: ` + error
+    console.trace(errorMessage)
+    return false
+  }
+}
+
+//generateRSS().then(_ => console.log(JSON.stringify(_, null, 2)))
